db92a57810eec87ad261271ad2fa305860ad624f
colteo <colazzomatteo@gmail.com>
2024-11-30 16:01:20 +0100

    Aggiunto controllo sulla variabile word_pattern

--- base/word.py
+++ base/word.py
@@ -15,9 +15,12 @@ class Word:
     def to_dict(self) -> Dict:
         """
         Converte l'oggetto Word in un dizionario.
-        Include il word_pattern tra parentesi tonde nel campo text.
+        Include il word_pattern tra parentesi tonde nel campo clue solo se presente.
         """
         base_dict = asdict(self)
-        # Modifica il campo clue per includere il word_pattern tra parentesi
-        base_dict['clue'] = f"{self.clue} ({self.word_pattern})"
+        # Modifica il campo clue per includere il word_pattern tra parentesi solo se presente
+        if self.word_pattern:
+            base_dict['clue'] = f"{self.clue} ({self.word_pattern})"
+        else:
+            base_dict['clue'] = self.clue
         return base_dict
